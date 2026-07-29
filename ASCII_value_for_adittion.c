9509fcac4296785f06e4ab858f7b698e41ab2a35
sivabalaji2715 <sakthirathi77@gmail.com>
2026-07-29 09:55:47 +0530

    Update ASCII_value_for_adittion.c

--- ASCII_value_for_adittion.c
+++ ASCII_value_for_adittion.c
@@ -10,7 +10,7 @@ int main() {
     printf("Enter second character: ");
     scanf(" %c", &ch2);
 
-    sum = ch1 + ch2;   // Adds ASCII values of the characters
+    sum = ch1 + ch2;   
 
     printf("ASCII value of '%c' = %d\n", ch1, ch1);
     printf("ASCII value of '%c' = %d\n", ch2, ch2);
