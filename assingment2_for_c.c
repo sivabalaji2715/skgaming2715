0cc456fd1f7ff484906a0aaa38de371d4d4eb5cf
sivabalaji2715 <sakthirathi77@gmail.com>
2026-07-29 09:55:20 +0530

    Update assingment2_for_c.c

--- assingment2_for_c.c
+++ assingment2_for_c.c
@@ -1,15 +1,15 @@
-#include <stdio.h>   // Header file
+#include <stdio.h>   
 
 int main() {
-    // Identifiers
+    
     int age = 20;
     float salary = 25000.50;
     char grade = 'A';
 
-    // Constants
+
     const float PI = 3.14;
 
-    // Display the values
+    
     printf("Age = %d\n", age);
     printf("Salary = %.2f\n", salary);
     printf("Grade = %c\n", grade);
@@ -17,10 +17,6 @@ int main() {
 
     return 0;
 }
-Sample Output
-Age = 20
-Salary = 25000.50
-Grade = A
-PI = 3.14
+
 
 
