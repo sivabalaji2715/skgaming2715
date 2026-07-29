eac622583099a575733c968f672fa7e810cee278
sivabalaji2715 <sakthirathi77@gmail.com>
2026-07-29 10:00:29 +0530

    Update operaters_program_c.c

--- operaters_program_c.c
+++ operaters_program_c.c
@@ -3,11 +3,11 @@
 int main() {
     int a, b;
 
-    // Input two numbers
+    
     printf("Enter two integers: ");
     scanf("%d %d", &a, &b);
 
-    // Arithmetic Operators
+
     printf("\nArithmetic Operators:\n");
     printf("Addition       = %d\n", a + b);
     printf("Subtraction    = %d\n", a - b);
@@ -15,7 +15,7 @@ int main() {
     printf("Division       = %d\n", a / b);
     printf("Modulus        = %d\n", a % b);
 
-    // Relational Operators
+    
     printf("\nRelational Operators:\n");
     printf("a == b : %d\n", a == b);
     printf("a != b : %d\n", a != b);
@@ -24,7 +24,7 @@ int main() {
     printf("a >= b : %d\n", a >= b);
     printf("a <= b : %d\n", a <= b);
 
-    // Logical Operators
+    
     printf("\nLogical Operators:\n");
     printf("(a > 0 && b > 0) : %d\n", (a > 0 && b > 0));
     printf("(a > 0 || b > 0) : %d\n", (a > 0 || b > 0));
@@ -32,23 +32,7 @@ int main() {
 
     return 0;
 }
-Sample Output
-Enter two integers: 10 5
 
-Arithmetic Operators:
-Addition       = 15
-Subtraction    = 5
-Multiplication = 50
-Division       = 2
-Modulus        = 0
-
-Relational Operators:
-a == b : 0
-a != b : 1
-a > b  : 1
-a < b  : 0
-a >= b : 1
-a <= b : 0
 
 Logical Operators:
 (a > 0 && b > 0) : 1
