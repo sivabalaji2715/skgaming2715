d7b3aa86ad4471694565e15383129773db7906a1
sivabalaji2715 <sakthirathi77@gmail.com>
2026-08-05 16:49:14 +0530

    Update types_of_casting_c.c

--- types_of_casting_c.c
+++ types_of_casting_c.c
@@ -4,13 +4,11 @@ int main() {
     float num = 15.75;
     int result;
 
-    result = (int)num;   // Explicit type casting
+    result = (int)num;   
 
     printf("Original float value : %.2f\n", num);
     printf("After type casting to int : %d\n", result);
 
     return 0;
 }
-Output
-Original float value : 15.75
-After type casting to int : 15
+
